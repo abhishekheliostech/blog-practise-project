a0e45c540179c8beaea23f19b53087461b5934ef
abhishek <abhisheksharma@thinglogix.com>
2019-07-30 16:27:54 +0530

    cors configured

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const bodyParser = require('body-parser')
 const posts = require('./routes/posts.routes')
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 // Set up mongoose connection
 const mongoDB = process.env.MONGODB_URI;
+
 mongoose.connect(mongoDB,{useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 //
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/posts', posts);
