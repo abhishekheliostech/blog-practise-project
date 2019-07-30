51df6bcb03532ad0eb2eda2a2ce5a68d341648a1
abhishek <abhisheksharma@thinglogix.com>
2019-07-30 14:28:51 +0530

    changed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const posts = require('./routes/posts.routes')
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 // Set up mongoose connection
 let dev_db_url = 'mongodb+srv://abhishektest:helios%23123@cluster0-vzlsd.mongodb.net/test?retryWrites=true&w=majority';
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
